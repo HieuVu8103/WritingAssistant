049403feb3cc24d55f90684baa7639d9bd97b0ff
Nghia <hieuvu8103@gmail.com>
2024-06-10 17:41:48 +0700

    Authenticate & Activity history

--- screens/Account.js
+++ screens/Account.js
@@ -1,36 +1,77 @@
-import React, { useState, useContext } from 'react';
-import { View, Text, StyleSheet, FlatList, Image, TouchableOpacity } from 'react-native';
-import { AuthContext } from '../components/AuthContext';
-import { Ionicons } from '@expo/vector-icons';
+import React, { useState, useEffect, useCallback } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  Image,
+  TouchableOpacity,
+} from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import {
+  getFirestore,
+  collection,
+  getDoc,
+  getDocs,
+  doc,
+} from "firebase/firestore";
+import { get } from "react-native-clipboard";
+import { useFocusEffect } from "@react-navigation/native";
 
-const Account = () => {
-  const [activityHistory, setActivityHistory] = useState([
-    { id: 1, input: 'This is a sample input text.', output: 'This is the corresponding output.' },
-    { id: 2, input: 'Another example input.', output: 'Another example output.' },
-  ]);
+const Account = ({ navigation }) => {
+  const db = getFirestore();
 
-  const userData = {
-    name: 'Hiếu Vũ',
-    email: 'hieu.vu@example.com',
-    photo: 'https://scontent.fhan5-9.fna.fbcdn.net/v/t39.30808-1/431246820_1453386681926182_5806316568775742809_n.jpg?stp=dst-jpg_p200x200&_nc_cat=109&ccb=1-7&_nc_sid=5f2048&_nc_ohc=1gePTyOGMEgQ7kNvgGpiC_3&_nc_ht=scontent.fhan5-9.fna&oh=00_AYA1Va_KpVMV0K-9Lk7rKwzQK4_j0vLJbpPt8hVtsexzbw&oe=665FE1CA',
-  };
+  const [uid, setUid] = useState(null);
+  const auth = getAuth();
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      setUid(user.uid);
+    }
+  });
 
-  const { logout } = useContext(AuthContext);
+  const [userData, setUserData] = useState({});
 
-  // Hàm để thêm một mục mới vào lịch sử hoạt động
-  const addToActivityHistory = (input, output) => {
-    const newActivity = {
-      id: Math.random().toString(), // Tạo một id ngẫu nhiên
-      input: input,
-      output: output,
-    };
-    setActivityHistory(prevActivity => [newActivity, ...prevActivity]);
+  const [activityHistory, setActivityHistory] = useState([]);
+  useFocusEffect(
+    useCallback(() => {
+      const fetchData = async () => {
+        if (uid) {
+          const docsRef = await getDocs(collection(db, "database"));
+          const userActivities = [];
+          docsRef.forEach((doc) => {
+            if (doc.data().id === uid) {
+              console.log(doc.id, " => ", doc.data());
+              userActivities.push({
+                id: doc.id,
+                input: doc.data().input,
+                output: doc.data().output,
+              });
+            }
+          });
+          const userRef = await getDocs(collection(db, "user"));
+          userRef.forEach((doc) => {
+            if (doc.data().id === uid) {
+              setUserData(doc.data());
+            }
+          });
+          setActivityHistory(userActivities);
+        }
+      };
+
+      fetchData();
+    }, [uid, db])
+  );
+  // console.log(userData);
+
+  const logout = () => {
+    auth.signOut();
+    navigation.navigate("Login");
   };
 
   return (
     <View style={styles.container}>
       <View style={styles.userInfo}>
-        <Image source={{ uri: userData.photo }} style={styles.photo} />
         <View>
           <Text style={styles.name}>{userData.name}</Text>
           <Text style={styles.email}>{userData.email}</Text>
@@ -60,13 +101,14 @@ const Account = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     padding: 16,
   },
   userInfo: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
     marginBottom: 16,
+    
   },
   photo: {
     width: 80,
@@ -76,46 +118,52 @@ const styles = StyleSheet.create({
   },
   name: {
     fontSize: 18,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   email: {
     fontSize: 16,
-    color: '#666',
+    color: "#666",
   },
   dashboardContainer: {
     flex: 1,
     borderWidth: 1,
-    borderColor: '#ccc',
+    borderColor: "#ccc",
     borderRadius: 8,
     padding: 16,
   },
   dashboardTitle: {
     fontSize: 18,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 8,
   },
   activityItem: {
     marginBottom: 16,
+    borderWidth: 1, 
+    borderRadius: 10,
+    padding: 10,
+    width: 340,
   },
   input: {
     fontSize: 16,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   output: {
     fontSize: 16,
-    color: '#666',
+    color: "#666",
   },
   logoutButton: {
-    backgroundColor: 'red',
+    backgroundColor: "red",
     padding: 5,
     borderRadius: 5,
     marginLeft: 89,
-    marginBottom: 18
+    marginBottom: 18,
+    position: "absolute",
+    right: 10
   },
   logoutButtonText: {
-    color: 'white',
-    fontWeight: 'bold',
-    textAlign: 'center',
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center",
   },
 });
 
--- screens/Login.js
+++ screens/Login.js
@@ -1,49 +1,162 @@
-import React, { useState, useContext } from 'react';
-import { View, Text, TextInput, TouchableOpacity, StyleSheet, KeyboardAvoidingView, Platform } from 'react-native';
-import { TouchableWithoutFeedback, Keyboard } from 'react-native';
-import { AuthContext } from '../components/AuthContext';
+import React, { useState, useEffect } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+  Keyboard,
+  StyleSheet,
+  Platform,
+  ScrollView,
+  Alert,
+} from "react-native";
+import { initializeApp } from "@firebase/app";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+  initializeAuth,
+  getReactNativePersistence,
+} from "@firebase/auth";
+import { getFirestore, collection, addDoc } from "firebase/firestore";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-const LoginScreen = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const { login } = useContext(AuthContext);
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCiNpznnweRhUh8Uxv8hAU1mPzd6dwX2z4",
+  authDomain: "writingassistant-66532.firebaseapp.com",
+  databaseURL: "https://writingassistant-66532-default-rtdb.firebaseio.com",
+  projectId: "writingassistant-66532",
+  storageBucket: "writingassistant-66532.appspot.com",
+  messagingSenderId: "274593024097",
+  appId: "1:274593024097:web:836a622573cabe0a7e8220"
+};
+const app = initializeApp(firebaseConfig);
+const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(AsyncStorage),
+});
+const LoginScreen = ({ navigation }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [isLogin, setIsLogin] = useState(true); // Track whether it's login or register
+  const [realName, setRealName] = useState("");
 
-  const handleLogin = () => {
-    // Xử lý logic đăng nhập ở đây
-    console.log('Email:', email);
-    console.log('Password:', password);
+  const db = getFirestore();
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        console.log("User login!");
+        navigation.navigate("Home");
+      }
+    });
 
-    login();
+    return () => unsubscribe();
+  }, [auth]);
+
+  const handleAuthentication = async () => {
+    try {
+      if (isLogin) {
+        await signInWithEmailAndPassword(auth, email, password);
+        console.log("User signed in successfully!");
+      } else {
+        if (password !== confirmPassword) {
+          console.error("Passwords do not match!");
+          Alert.alert("Passwords do not match!");
+          return;
+        }
+        await createUserWithEmailAndPassword(auth, email, password);
+        const docRef = await addDoc(collection(db, "user"), {
+          id: auth.currentUser.uid,
+          email,
+          name: realName,
+        });
+        // console.log("User created successfully!");
+        // setIsLogin(true);
+        // setEmail("");
+        // setPassword("");
+        // setConfirmPassword("");
+        // setRealName("");
+      }
+    } catch (error) {
+      if (error.code === "auth/email-already-in-use") {
+        Alert.alert("That email address is already in use!");
+      } else if (error.code === "auth/invalid-email") {
+        Alert.alert("That email address is invalid!");
+      } else if (error.code === "auth/weak-password") {
+        Alert.alert("Password is too weak!");
+      } else if (error.code === "auth/invalid-credential") {
+        Alert.alert("Wrong email or password!");
+      } else {
+        console.error(error.code);
+        Alert.alert("An error occurred. Please try again later.");
+      }
+    }
   };
 
   return (
     <KeyboardAvoidingView
       style={styles.container}
-      behavior={Platform.OS === 'ios' ? 'padding' : undefined}
-      keyboardVerticalOffset={Platform.OS === 'ios' ? 40 : 0}
+      behavior={Platform.OS === "ios" ? "padding" : undefined}
+      keyboardVerticalOffset={Platform.OS === "ios" ? 40 : 0}
     >
       <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-        <View style={styles.content}>
-          <Text style={styles.title}>Login</Text>
+        <ScrollView contentContainerStyle={styles.content}>
+          <Text style={styles.title}>{isLogin ? "Login" : "Register"}</Text>
           <TextInput
             style={styles.input}
             placeholder="Email"
             value={email}
-            onChangeText={(text) => setEmail(text)}
+            onChangeText={setEmail}
             autoCapitalize="none"
             keyboardType="email-address"
           />
+          {!isLogin && (
+            <TextInput
+              style={styles.input}
+              placeholder="Real Name"
+              value={realName}
+              onChangeText={setRealName}
+            />
+          )}
           <TextInput
             style={styles.input}
             placeholder="Password"
             value={password}
-            onChangeText={(text) => setPassword(text)}
+            onChangeText={setPassword}
             secureTextEntry
           />
-          <TouchableOpacity style={styles.button} onPress={handleLogin}>
-            <Text style={styles.buttonText}>Login</Text>
+          {!isLogin && (
+            <View>
+              <TextInput
+                style={styles.input}
+                placeholder="Confirm Password"
+                value={confirmPassword}
+                onChangeText={setConfirmPassword}
+                secureTextEntry
+              />
+            </View>
+          )}
+          <TouchableOpacity
+            style={styles.button}
+            onPress={handleAuthentication}
+          >
+            <Text style={styles.buttonText}>
+              {isLogin ? "Login" : "Register and Login"}
+            </Text>
           </TouchableOpacity>
-        </View>
+
+          <TouchableOpacity onPress={() => setIsLogin(!isLogin)}>
+            <Text style={styles.toggleText}>
+              {isLogin
+                ? "Need an account? Register"
+                : "Already have an account? Login"}
+            </Text>
+          </TouchableOpacity>
+        </ScrollView>
       </TouchableWithoutFeedback>
     </KeyboardAvoidingView>
   );
@@ -52,37 +165,43 @@ const LoginScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
   },
   content: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: "center",
     paddingHorizontal: 16,
   },
   title: {
     fontSize: 24,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 24,
-    textAlign: 'center',
+    textAlign: "center",
   },
   input: {
     height: 48,
     borderWidth: 1,
-    borderColor: '#ccc',
+    borderColor: "#ccc",
     borderRadius: 4,
     paddingHorizontal: 12,
     marginBottom: 16,
   },
   button: {
     height: 48,
-    backgroundColor: '#2CB673',
+    backgroundColor: "#2CB673",
     borderRadius: 4,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
   buttonText: {
-    color: '#fff',
-    fontWeight: 'bold',
+    color: "#fff",
+    fontWeight: "bold",
+  },
+  toggleText: {
+    marginTop: 16,
+    color: "#2CB673",
+    textAlign: "center",
+    fontWeight: "bold",
   },
 });
 
