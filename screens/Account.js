d8e72166d983681d1a2b4991aace78651e7947c7
Hiếu Vũ <166103520+HieuVu8103@users.noreply.github.com>
2024-06-14 20:04:36 +0700

    Update Account.js

--- screens/Account.js
+++ screens/Account.js
@@ -4,25 +4,23 @@ import {
   Text,
   StyleSheet,
   FlatList,
-  Image,
   TouchableOpacity,
+  ActivityIndicator,
 } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import {
   getFirestore,
   collection,
-  getDoc,
   getDocs,
-  doc,
 } from "firebase/firestore";
-import { get } from "react-native-clipboard";
 import { useFocusEffect } from "@react-navigation/native";
 
 const Account = ({ navigation }) => {
   const db = getFirestore();
 
   const [uid, setUid] = useState(null);
+  const [loading, setLoading] = useState(false); 
   const auth = getAuth();
   onAuthStateChanged(auth, (user) => {
     if (user) {
@@ -31,38 +29,44 @@ const Account = ({ navigation }) => {
   });
 
   const [userData, setUserData] = useState({});
-
   const [activityHistory, setActivityHistory] = useState([]);
+
   useFocusEffect(
     useCallback(() => {
       const fetchData = async () => {
         if (uid) {
-          const docsRef = await getDocs(collection(db, "database"));
-          const userActivities = [];
-          docsRef.forEach((doc) => {
-            if (doc.data().id === uid) {
-              console.log(doc.id, " => ", doc.data());
-              userActivities.push({
-                id: doc.id,
-                input: doc.data().input,
-                output: doc.data().output,
-              });
-            }
-          });
-          const userRef = await getDocs(collection(db, "user"));
-          userRef.forEach((doc) => {
-            if (doc.data().id === uid) {
-              setUserData(doc.data());
-            }
-          });
-          setActivityHistory(userActivities);
+          setLoading(true);
+          try {
+            const docsRef = await getDocs(collection(db, "database"));
+            const userActivities = [];
+            docsRef.forEach((doc) => {
+              if (doc.data().id === uid) {
+                console.log(doc.id, " => ", doc.data());
+                userActivities.push({
+                  id: doc.id,
+                  input: doc.data().input,
+                  output: doc.data().output,
+                });
+              }
+            });
+            const userRef = await getDocs(collection(db, "user"));
+            userRef.forEach((doc) => {
+              if (doc.data().id === uid) {
+                setUserData(doc.data());
+              }
+            });
+            setActivityHistory(userActivities);
+          } catch (error) {
+            console.error("Error fetching data:", error);
+          } finally {
+            setLoading(false); 
+          }
         }
       };
 
       fetchData();
     }, [uid, db])
   );
-  // console.log(userData);
 
   const logout = () => {
     auth.signOut();
@@ -83,16 +87,20 @@ const Account = ({ navigation }) => {
 
       <View style={styles.dashboardContainer}>
         <Text style={styles.dashboardTitle}>Activity Dashboard</Text>
-        <FlatList
-          data={activityHistory}
-          keyExtractor={(item) => item.id}
-          renderItem={({ item }) => (
-            <View style={styles.activityItem}>
-              <Text style={styles.input}>Input: {item.input}</Text>
-              <Text style={styles.output}>Output: {item.output}</Text>
-            </View>
-          )}
-        />
+        {loading ? (
+          <ActivityIndicator size="large" color="#2CB673" />
+        ) : (
+          <FlatList
+            data={activityHistory}
+            keyExtractor={(item) => item.id}
+            renderItem={({ item }) => (
+              <View style={styles.activityItem}>
+                <Text style={styles.input}>Input: {item.input}</Text>
+                <Text style={styles.output}>Output: {item.output}</Text>
+              </View>
+            )}
+          />
+        )}
       </View>
     </View>
   );
@@ -108,13 +116,6 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     alignItems: "center",
     marginBottom: 16,
-    
-  },
-  photo: {
-    width: 80,
-    height: 80,
-    borderRadius: 40,
-    marginRight: 16,
   },
   name: {
     fontSize: 18,
@@ -138,10 +139,9 @@ const styles = StyleSheet.create({
   },
   activityItem: {
     marginBottom: 16,
-    borderWidth: 1, 
+    borderWidth: 1,
     borderRadius: 10,
     padding: 10,
-    width: 340,
   },
   input: {
     fontSize: 16,
@@ -155,10 +155,8 @@ const styles = StyleSheet.create({
     backgroundColor: "red",
     padding: 5,
     borderRadius: 5,
-    marginLeft: 89,
-    marginBottom: 18,
     position: "absolute",
-    right: 10
+    right: 10,
   },
   logoutButtonText: {
     color: "white",
